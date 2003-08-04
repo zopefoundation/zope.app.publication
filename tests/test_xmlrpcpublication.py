d66236836f169ab8ee5be1a33a378524350ee441
Stephan Richter <stephan.richter@gmail.com>
2003-08-04 23:19:25 +0000

    After wanting to fix XML-RPC for over a year, I finally got around to do it. You can now specify view names, create default views and of course you do not have access to the object's methods directly anymore.
    Added some tests for all of this.
    
    Updated XML-RPC directives. Made them also a bit more consistent with the
    browser ones.
    
    This should make the XML-RPC code ready for the beta.

---
+++ tests/test_xmlrpcpublication.py
@@ -0,0 +1,166 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""XML-RPC Publication Tests
+
+$Id: test_xmlrpcpublication.py,v 1.1 2003/08/04 23:19:07 srichter Exp $
+"""
+import unittest
+
+from zope.app.publication.tests.test_zopepublication import \
+     BasePublicationTests
+from zope.app.publication.traversers import TestTraverser
+from zope.app.publication.xmlrpc import XMLRPCPublication
+from zope.app.services.servicenames import Views
+from zope.component import getService
+from zope.context import getWrapperContext
+from zope.interface import Interface, implements
+from zope.proxy import removeAllProxies
+from zope.publisher.interfaces import NotFound
+from zope.publisher.interfaces.xmlrpc import IXMLRPCPresentation
+from zope.publisher.xmlrpc import TestRequest
+
+
+class SimpleObject:
+    def __init__(self, v):
+        self.v = v
+
+
+class XMLRPCPublicationTests(BasePublicationTests):
+
+    klass = XMLRPCPublication
+
+    def _createRequest(self, path, publication, **kw):
+        request = TestRequest(PATH_INFO=path, **kw)
+        request.setPublication(publication)
+        return request
+
+    def testTraverseName(self):
+        pub = self.klass(self.db)
+        class C:
+            x = SimpleObject(1)
+        ob = C()
+        r = self._createRequest('/x', pub)
+        provideView=getService(None, Views).provideView
+        provideView(None, '_traverse', IXMLRPCPresentation, [TestTraverser])
+        ob2 = pub.traverseName(r, ob, 'x')
+        self.assertEqual(removeAllProxies(ob2).v, 1)
+        self.assertEqual(getWrapperContext(ob2), ob)
+
+    def testDenyDirectMethodAccess(self):
+        pub = self.klass(self.db)
+        class I(Interface):
+            pass
+
+        class C:
+            implements(I)
+
+            def foo(self):
+                return 'bar'
+
+        class V:
+            def __init__(self, context, request):
+                pass
+            implements(IXMLRPCPresentation)
+
+        ob = C()
+        r = self._createRequest('/foo', pub)
+        provideView=getService(None, Views).provideView
+        setDefaultViewName=getService(None, Views).setDefaultViewName
+        provideView(I, 'view', IXMLRPCPresentation, [V])
+        setDefaultViewName(I, IXMLRPCPresentation, 'view')
+        self.assertRaises(NotFound, pub.traverseName, r, ob, 'foo')
+
+
+    def testTraverseNameView(self):
+        pub = self.klass(self.db)
+
+        class I(Interface):
+            pass
+
+        class C:
+            implements(I)
+
+        ob = C()
+
+        class V:
+            def __init__(self, context, request):
+                pass
+            implements(IXMLRPCPresentation)
+
+        r = self._createRequest('/@@spam', pub)
+        provideView=getService(None, Views).provideView
+        provideView(I, 'spam', IXMLRPCPresentation, [V])
+        ob2 = pub.traverseName(r, ob, '@@spam')
+        self.assertEqual(removeAllProxies(ob2).__class__, V)
+        self.assertEqual(getWrapperContext(ob2), ob)
+
+        ob2 = pub.traverseName(r, ob, 'spam')
+        self.assertEqual(removeAllProxies(ob2).__class__, V)
+        self.assertEqual(getWrapperContext(ob2), ob)
+
+
+    def testTraverseNameDefaultView(self):
+        pub = self.klass(self.db)
+
+        class I(Interface):
+            pass
+
+        class C:
+            implements(I)
+
+        ob = C()
+
+        class V:
+            implements(IXMLRPCPresentation)
+
+            def __init__(self, context, request):
+                pass
+
+            def spam(self):
+                return 'foo'
+
+        r = self._createRequest('/spam', pub)
+        provideView=getService(None, Views).provideView
+        setDefaultViewName=getService(None, Views).setDefaultViewName
+        provideView(I, 'view', IXMLRPCPresentation, [V])
+        setDefaultViewName(I, IXMLRPCPresentation, 'view')
+
+        ob2 = pub.traverseName(r, ob, '@@spam')
+        self.assertEqual(removeAllProxies(ob2).__name__, V.spam.__name__)
+        self.assertEqual(getWrapperContext(ob2), ob)
+
+        ob2 = pub.traverseName(r, ob, 'spam')
+        self.assertEqual(removeAllProxies(ob2).__name__, V.spam.__name__)
+        self.assertEqual(getWrapperContext(ob2), ob)
+
+
+    def testTraverseNameServices(self):
+        pub = self.klass(self.db)
+        class C:
+            def getServiceManager(self):
+                return SimpleObject(1)
+        ob = C()
+        r = self._createRequest('/++etc++site',pub)
+        ob2 = pub.traverseName(r, ob, '++etc++site')
+        self.assertEqual(removeAllProxies(ob2).v, 1)
+        self.assertEqual(getWrapperContext(ob2), ob)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(XMLRPCPublicationTests),
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
--- xmlrpc.py
+++ xmlrpc.py
@@ -11,33 +11,73 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""
+"""XML-RPC Publication Handler.
 
-$Id: xmlrpc.py,v 1.6 2003/05/28 15:46:10 jim Exp $
-"""
+This module specifically implements a custom nameTraverse() method.
 
-from zope.proxy import removeAllProxies
+$Id: xmlrpc.py,v 1.7 2003/08/04 23:19:04 srichter Exp $
+"""
+from zope.app.context import ContextWrapper
 from zope.app.publication.zopepublication import ZopePublication
-from zope.component import queryView
+from zope.component import queryView, queryDefaultViewName
+from zope.proxy import removeAllProxies
+from zope.publisher.interfaces.xmlrpc import IXMLRPCPresentation
+from zope.security.checker import ProxyFactory
 
 class XMLRPCPublication(ZopePublication):
-    """XML-RPC publication handling.
-
-       There is nothing special here right now.
-    """
+    """XML-RPC publication handling."""
 
     def traverseName(self, request, ob, name):
+        """Traverse the name.
 
+        The method should try the following things in this order:
+
+        1. Check whether ob is a view; if so try to find a public method
+           having the passed name.
+
+        2. If the ob is not a view, then we try to find a view for it. This
+           can be done in two ways:
+
+           (a) Look whether there is a view registered for this name.
+
+           (b) Check whether the default view has a matching method called name.
+
+        3. See whether the object has a subobject of this name. This test is
+           done last, since this is done by ZopePublication, and it knows how
+           to create all the correct error messages. No need for us to do that.
+
+        """
         naked_ob = removeAllProxies(ob)
-        view = queryView(ob, 'methods', request, self)
 
-        if hasattr(ob, name):
-            return getattr(ob, name)
-        if view is not self and hasattr(view, name):
-            return getattr(view, name)
-        else:
-            return super(XMLRPCPublication, self).traverseName(request,
-                                                               ob, name)
+        # Use the real view name
+        view_name = name
+        if view_name.startswith('@@'):
+            view_name = view_name[2:]
+
+        # If ob is a presentation object, then we just get the method
+        if IXMLRPCPresentation.isImplementedBy(naked_ob) and \
+               hasattr(ob, view_name):
+            return ProxyFactory(
+                ContextWrapper(getattr(ob, view_name), ob, name=view_name))
+
+        # Let's check whether name could be a view 
+        view = queryView(ob, view_name, request)
+        if view is not None:
+            return ProxyFactory(ContextWrapper(view, ob, name=view_name))
+
+        # Now let's see whether we have a default view with a matching method
+        # name
+        defaultName = queryDefaultViewName(ob, request)
+
+        if defaultName is not None:
+            view = queryView(ob, defaultName, request, object)
+            if hasattr(view, view_name):
+                return ProxyFactory(ContextWrapper(getattr(view, view_name), ob,
+                                                   name=view_name))
+
+        # See whether we have a subobject
+        return super(XMLRPCPublication, self).traverseName(request, ob, name)
+
 
 # For now, have a factory that returns a singleton
 class XMLRPCPublicationFactory:
