8035ca4bfb0b77871a8db16e49339dfb8b1e26db
Jim Fulton <jim@zope.com>
2004-05-26 17:01:20 +0000

    M    src/zope/app/observable/configure.zcml M    src/zope/app/undo/configure.zcml M    src/zope/app/dublincore/configure.zcml M    src/zope/app/appsetup/configure.zcml M    src/zope/app/event/configure.zcml M    src/zope/app/container/configure.zcml M    src/zope/app/module/configure.zcml M    src/zope/app/wiki/configure.zcml
    Changd event handler configurations to use subscription adapters.
    
    M    src/zope/app/undo/__init__.py
    M    src/zope/app/servicenames.py
    M    src/zope/app/dublincore/tests/test_creatorannotator.py
    M    src/zope/app/workflow/interfaces/__init__.py
    M    src/zope/app/workflow/stateful/tests/test_instance.py
    M    src/zope/app/workflow/stateful/tests/test_contentworkflow.py
    M    src/zope/app/presentation/presentation.py
    M    src/zope/app/broken/broken.py
    M    src/zope/app/appsetup/bootstrap.py
    M    src/zope/app/appsetup/appsetup.py
    M    src/zope/app/appsetup/tests.py
    M    src/zope/app/rotterdam/tests/test_xmlnavigationviews.py
    M    src/zope/app/tests/setup.py
    M    src/zope/app/event/tests/placelesssetup.py
    M    src/zope/app/mail/interfaces.py
    M    src/zope/app/module/__init__.py
    M    src/zope/app/publication/interfaces.py
    
    Removed uses of IEvent and removd references to event services.

---
+++ interfaces.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+
+$Id$
+"""
+
+from zope.interface import implements, Interface
+
+class IPublicationRequestFactoryFactory(Interface):
+    """Publication request factory factory"""
+
+    def realize(db):
+        """Create a publication and request factory for a given database
+
+        Return a IPublicationRequestFactory for the given database.
+        """
+
+
+class IPublicationRequestFactory(Interface):
+    """Publication request factory"""
+
+    def __call__(input_stream, output_steam, env):
+        """Create a request object to handle the given inputs
+
+        A request is created and configured with a publication object.
+        """
+
+
+class IRequestFactory(IPublicationRequestFactory,
+                      IPublicationRequestFactoryFactory):
+    """This is a pure read-only interface, since the values are set through
+       a ZCML directive and we shouldn't be able to change them.
+    """
+
+
+class IBeforeTraverseEvent(Interface):
+    """An event which gets sent on publication traverse"""
+
+
+class BeforeTraverseEvent:
+    """An event which gets sent on publication traverse"""
+    implements(IBeforeTraverseEvent)
+    def __init__(self, ob, request):
+        self.object = ob
+        self.request = request
+
+
+class IEndRequestEvent(Interface):
+    """An event which gets sent when the publication is ended"""
+
+
+class EndRequestEvent:
+    """An event which gets sent when the publication is ended"""
+    implements(IEndRequestEvent)
+    def __init__(self, ob, request):
+        self.object = ob
+        self.request = request
