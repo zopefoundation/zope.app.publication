d39a14560cfdfee1e45e623220ed3af37f66c69b
Marius Gedminas <marius@gedmin.as>
2004-06-11 18:22:00 +0000

    Improved content type guessing heuristic in zope.publisher.browser.  Now source annotations will not result in som epages being returned as text/plain.
    Removed comments implicating Mozilla which is completely innocent.  Removed
    code that tried to work around this bug by postponing annotations after
    doctype declarations.  Made the content-type guesser add a header
    
      X-Content-Type-Warning: guessed from content
    
    In the future all page templates should declare their content types explicitly
    and if they do not, a warning should be displayed in the event log.

--- tests/test_browserpublication.py
+++ tests/test_browserpublication.py
@@ -287,6 +287,7 @@ class BrowserPublicationTests(BasePublicationTests):
             'Status: 200 Ok\r\n'
             'Content-Length: 4\r\n'
             'Content-Type: text/plain;charset=utf-8\r\n'
+            'X-Content-Type-Warning: guessed from content\r\n'
             'X-Powered-By: Zope (www.zope.org), Python (www.python.org)\r\n'
             '\r\nspam'
             )
@@ -304,6 +305,7 @@ class BrowserPublicationTests(BasePublicationTests):
             'Status: 200 Ok\r\n'
             'Content-Length: 0\r\n'
             'Content-Type: text/plain;charset=utf-8\r\n'
+            'X-Content-Type-Warning: guessed from content\r\n'
             'X-Powered-By: Zope (www.zope.org), Python (www.python.org)\r\n'
             '\r\n'
             )
@@ -319,6 +321,7 @@ class BrowserPublicationTests(BasePublicationTests):
             'Status: 200 Ok\r\n'
             'Content-Length: 8\r\n'
             'Content-Type: text/plain;charset=utf-8\r\n'
+            'X-Content-Type-Warning: guessed from content\r\n'
             'X-Powered-By: Zope (www.zope.org), Python (www.python.org)\r\n'
             '\r\n\xd1\x82\xd0\xb5\xd1\x81\xd1\x82')
 
