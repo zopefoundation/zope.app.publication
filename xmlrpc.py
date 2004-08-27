28f18b98c72b90ce7a446127b0a5b81f4d0e0257
Jim Fulton <jim@zope.com>
2004-08-27 21:22:43 +0000

    Merged from ZopeX3-3.0 branch:
      r27290 | jim | 2004-08-27 10:28:21 -0400 (Fri, 27 Aug 2004) | 12 lines
    
    Refactored xmlrpc:
    
    - Made it possible to define xml-rpc methods on content. This made the
      "default view" mechanism unnecessary.
    
    - Removed the default-view mechanism.  This is backward incompatable, 
      but it's better to do this now, rather than after X3.0.
    
    This allowed the custom publication object to basically go away.
    
    - Added documentation in the form of a doctest.

--- xmlrpc.py
+++ xmlrpc.py
@@ -19,67 +19,11 @@ $Id$
 """
 __docformat__ = 'restructuredtext'
 
-from zope.component import queryView, queryDefaultViewName
-from zope.proxy import removeAllProxies
-from zope.app.publisher.interfaces.xmlrpc import IXMLRPCPresentation
-from zope.security.checker import ProxyFactory
 from zope.app.publication.http import BaseHTTPPublication
 
-class XMLRPCPublication(BaseHTTPPublication):
-    """XML-RPC publication handling."""
+# Don't need any special handling for xml-rpc
+XMLRPCPublication = BaseHTTPPublication
 
-    def traverseName(self, request, ob, name):
-        """Traverse the name.
-
-        The method should try the following things in this order:
-
-        1. Check whether `ob` is a view; if so try to find a public method
-           having the passed name.
-
-        2. If `ob` is not a view, then we try to find a view for it. This
-           can be done in two ways:
-
-           (a) Look whether there is a view registered for this name.
-
-           (b) Check whether the default view has a matching method called
-           name.
-
-        3. See whether the object has a subobject of this name. This test is
-           done last, since this is done by `ZopePublication`, and it knows how
-           to create all the correct error messages. No need for us to do that.
-
-        """
-        naked_ob = removeAllProxies(ob)
-
-        # Use the real view name
-        view_name = name
-        if view_name.startswith('@@'):
-            view_name = view_name[2:]
-
-        # If ob is a presentation object, then we just get the method
-        if (IXMLRPCPresentation.providedBy(naked_ob) and 
-            hasattr(ob, view_name)
-            ):
-            return ProxyFactory(getattr(ob, view_name))
-
-        # Let's check whether name could be a view 
-        view = queryView(ob, view_name, request)
-        if view is not None:
-            return ProxyFactory(view)
-
-        # Now let's see whether we have a default view with a matching method
-        # name
-        defaultName = queryDefaultViewName(ob, request)
-
-        if defaultName is not None:
-            view = queryView(ob, defaultName, request, object)
-            if hasattr(view, view_name):
-                return ProxyFactory(getattr(view, view_name))
-
-        # See whether we have a subobject
-        return super(XMLRPCPublication, self).traverseName(request, ob, name)
-
-# For now, have a factory that returns a singleton
 class XMLRPCPublicationFactory(object):
 
     def __init__(self, db):
