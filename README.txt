73a315e63a92f210dd6e17e753a7993dba4ec785
Roger Ineichen <roger@projekt01.ch>
2007-09-25 01:45:07 +0000

    Prepare for 3.4 egg

---
+++ README.txt
@@ -0,0 +1,1 @@
+Publication and traversal components.
--- setup.py
+++ setup.py
@@ -18,35 +18,55 @@ $Id$
 
 import os
 
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.publication',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.publication',
-      license='ZPL 2.1',
-      description='Zope publication',
-      author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['zope', 'zope.app'],
-      extras_require = dict(test=['zope.app.testing',
-                                  'zope.app.securitypolicy',
-                                  'zope.app.zcmlfiles',
-                                  'zope.app.dav',
-                                  'zope.app.zptpage',
-                                  ]),
-      install_requires=['zope.interface',
-                        'ZODB3',
-                        'zope.component',
-                        'zope.i18n',
-                        'zope.app.http',
-                        'zope.app.applicationcontrol',
-                        'zope.app.error',
-                        'zope.app.exception',
-                        'zope.app.security',
-                        'setuptools',
-                        ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+    version = '3.4.0',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    description='Zope publication',
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 publication",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://cheeseshop.python.org/pypi/zope.app.publication',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['zope', 'zope.app'],
+    extras_require = dict(
+        test=['zope.app.testing',
+              'zope.app.securitypolicy',
+              'zope.app.zcmlfiles',
+              'zope.app.dav',
+              'zope.app.zptpage',
+              ]),
+    install_requires=['zope.interface',
+                      'ZODB3',
+                      'zope.component',
+                      'zope.i18n',
+                      'zope.app.http',
+                      'zope.app.applicationcontrol',
+                      'zope.app.error',
+                      'zope.app.exception',
+                      'zope.app.security',
+                      'setuptools',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    )
