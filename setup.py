735aeef72f345112f26b2e98b1dd830e6b11b686
Thomas Lotze <tl@gocept.com>
2009-09-29 12:02:59 +0000

    Back to development: 3.9.1dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.9.0'
+version = '3.9.1dev'
 
 import os
 from setuptools import setup, find_packages
