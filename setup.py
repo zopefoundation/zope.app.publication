a24bfae849fe6c139c0e292975f0aa0ba383fa50
Hanno Schlichting <hanno@hannosch.eu>
2009-12-15 19:56:46 +0000

    Prepare zope.app.publication 3.10.0.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.10.0dev'
+version = '3.10.0'
 
 import os
 from setuptools import setup, find_packages
