809a3fc1370ce838ffa63baeb1c3569fbd66ef24
Sylvain Viollon <thefunny@gmail.com>
2010-04-13 16:27:34 +0000

    Update CHANGES.txt.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.11dev'
+version = '3.11.0dev'
 
 import os
 from setuptools import setup, find_packages
