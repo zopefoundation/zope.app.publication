72a9b1fc005856390e5b7e231c4372365aabd858
Martijn Faassen <faassen@startifact.com>
2010-04-19 17:53:04 +0000

    Back to development: 3.11.2dev

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.11.1'
+version = '3.11.2dev'
 
 import os
 from setuptools import setup, find_packages
