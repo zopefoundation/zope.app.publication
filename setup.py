b0646183b27e5b8c26fe36e0114d2001e2f12b0a
Michael Howitz <mh@gocept.com>
2011-01-25 10:10:20 +0000

    Back to development: 3.13.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.13.0'
+version = '3.13.1dev'
 
 import os
 from setuptools import setup, find_packages
