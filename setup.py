d853258c20908e05a4ae72c670a179916bff4e9b
Gediminas Paulauskas <menesis@pov.lt>
2011-08-04 15:20:50 +0000

    Preparing release 3.13.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.14dev'
+version = '3.13.2'
 
 import os
 from setuptools import setup, find_packages
