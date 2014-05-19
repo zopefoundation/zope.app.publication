d8682dc6b55e7516a12b16b689e0f302d291df5a
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2014-05-19 12:01:47 +0200

    Back to development: 3.15.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
 
-version = '3.15.0-md1'
+version = '3.15.0.dev0'
 
 import os
 from setuptools import setup, find_packages
