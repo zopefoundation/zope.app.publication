e89ffbe2cf090acc421dec31a22cbefdf00b6b62
Stephan Richter <stephan.richter@gmail.com>
2017-04-17 12:31:03 -0400

    Include manifest.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ TEST_REQUIREMENTS = [
 
 setup(
     name='zope.app.publication',
-    version='4.1.0',
+    version='4.1.1.dev0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope publication',
