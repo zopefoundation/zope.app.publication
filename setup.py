f3dc439db5b8a08ef9941838618d2d33df3e16b3
Marius Gedminas <marius@gedmin.as>
2019-07-01 17:23:45 +0300

    Back to development: 4.3.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ TEST_REQUIREMENTS = [
 
 setup(
     name='zope.app.publication',
-    version='4.3.1',
+    version='4.3.2.dev0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope publication',
