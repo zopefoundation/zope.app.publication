ebe6158ecc72255f01ff1b3193d6601a4d0144d5
Michael Howitz <mh@gocept.com>
2020-11-05 08:28:57 +0100

    Back to development: 4.6

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ TEST_REQUIREMENTS = [
 
 setup(
     name='zope.app.publication',
-    version='4.5',
+    version='4.6.dev0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope publication',
